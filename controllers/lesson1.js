ca9826cb4be371b7f145dc9ce730fb5d4a63a25d
Eric Arndt <earndt1701@gmail.com>
2025-10-30 09:41:25 -0700

    Going above for 100%

---
+++ controllers/lesson1.js
@@ -0,0 +1,18 @@
+const sarahRoute = (req, res) => {
+  res.send('Sarah Birch');
+}
+
+const emilyRoute = (req, res) => {
+  res.send('Emily Birch');
+}
+
+const hannahRoute = (req, res) => {
+  res.send('Hannah Birch');
+};
+
+
+module.exports = {
+  emilyRoute,
+  hannahRoute,
+  sarahRoute
+};
--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hannah Birch');
-});
+app.use('/', require('./routes'));
 
 app.listen(PORT, () => {
   console.log(`Web Server is listening at port ${PORT}`);
